b128db4b096ebe2c78601b3cff12bf0c19f2a4ec
Gloria Sun <53097343+gloriasun4@users.noreply.github.com>
2022-11-29 12:01:49 -0500

    Added search recipe function on add-recipe.php

--- add-recipe.php
+++ add-recipe.php
@@ -241,6 +241,81 @@ method: Allows yout to specify how the form data should be packaged
 </form>
 
 <hr/> <!-- Horizontal --> 
+<!-- Search function -->
+<form method="post" action="add-recipe.php">
+  <h2>Search for Recipes</h2>
+  <input type="text" name="search"/>
+  <input type="submit" value="Search"/>
+</form>
+
+<div class="row justify-content-center">  
+<table class="w3-table w3-bordered w3-card-4 center" style="width:70%">
+  <thead> <!-- For the table set up the header --> 
+  <tr style="background-color:#B0B0B0">
+      <th width="30%"><b>Recipe_id</b></th>       
+      <th width="30%"><b>Name</b></th>      
+      <th width="30%"><b>Instructions</b></th>  
+      <th><b>Update?</b></th>
+      <th><b>Delete?</b></th>
+  </tr>
+  </thead>
+
+<?php
+    // (B) PROCESS SEARCH WHEN FORM SUBMITTED
+    if (isset($_POST["search"])) {
+      // (B1) SEARCH FOR USERS
+      require "search.php";
+
+      // (B2) DISPLAY RESULTS
+      if (count($results) > 0) { 
+
+        // printf("<div>%s - %s</div>", $r["recipe_name"]);
+        
+        // printf($r["recipe_name"]);
+        foreach ($results as $recipe_info) { ?>
+        <tr>
+          <td><?php echo $recipe_info['recipe_id']; ?></td>
+          <td><?php echo $recipe_info['recipe_name']; ?></td>        
+    <td><?php echo $recipe_info['instructions']; ?></td>  
+    <td>
+      <form action="add-recipe.php" method="post">
+    <!-- As soon as the button is clicked, send a request to simpleform.php so it can update --> 
+      <input type="submit" value="Update" name="btnAction" class="btn btn-primary"
+        title="Click to update this recipe" /> <!-- title attribute will display when mouse hovers over it -->
+      <input type="hidden" name="recipe_to_update" 
+        value="<?php echo $recipe_info['recipe_name']; ?>"
+      />
+      <!-- hidden input is submitted when the form is submitted, but it's not shown on the screen --> 
+    </form>
+  </td> 
+  <!-- DELETE BUTTON --> 
+  <td><form action="add-recipe.php" method="post">
+    <!-- As soon as the button is clicked, send a request to simpleform.php so it can update --> 
+      <input type="submit" value="Delete" name="btnAction" class="btn btn-danger" 
+        title="Click to delete this recipe" /> <!-- title attribute will display when mouse hovers over it -->
+      <input type="hidden" name="recipe_to_delete" 
+        value="<?php echo $recipe_info['recipe_name']; ?>" 
+      />
+      <!-- hidden input is submitted when the form is submitted, but it's not shown on the screen --> 
+    </form></td>              
+        </tr>
+        <?php }
+
+    } else { ?>
+
+      <p>No results found.<br></p>
+      
+    
+    <?php
+    }
+    }
+?>
+</table>
+
+</div>
+
+<!-- Retrieve Recipes -->
+<hr/> <!-- Horizontal --> 
 <h3> List of Recipes</h3> 
 <div class="row justify-content-center">  
 <table class="w3-table w3-bordered w3-card-4 center" style="width:70%">
